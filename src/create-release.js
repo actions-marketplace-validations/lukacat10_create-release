24a40d97b18e3ebd48c8086d8a70d0d4f783e185
lukacat10 <lukacat100@gmail.com>
2020-02-25 09:15:53 +0200

    Added a csproj path optional input, which then goes to the csproj file and extracts the version

--- src/create-release.js
+++ src/create-release.js
@@ -15,9 +15,44 @@ async function run() {
     // This removes the 'refs/tags' portion of the string, i.e. from 'refs/tags/v1.10.15' to 'v1.10.15'
     const tag = tagName.replace('refs/tags/', '');
     const releaseName = core.getInput('release_name', { required: true }).replace('refs/tags/', '');
+    const csproj = core.getInput('csproj_path', {required: false });
     const body = core.getInput('body', { required: false });
     const draft = core.getInput('draft', { required: false }) === 'true';
     const prerelease = core.getInput('prerelease', { required: false }) === 'true';
+
+    if(csproj != null){
+      const contentsBuffer = fs.readFileSync(path);
+      const content = contentsBuffer.toString('utf-8');
+
+      var parser = require('fast-xml-parser');
+      var he = require("he");
+
+      var options = {
+        attributeNamePrefix : "@_",
+        attrNodeName: "attr", //default is 'false'
+        textNodeName : "#text",
+        ignoreAttributes : true,
+        ignoreNameSpace : false,
+        allowBooleanAttributes : false,
+        parseNodeValue : true,
+        parseAttributeValue : false,
+        trimValues: true,
+        cdataTagName: "__cdata", //default is 'false'
+        cdataPositionChar: "\\c",
+        parseTrueNumberOnly: false,
+        arrayMode: false, //"strict"
+        attrValueProcessor: (val, attrName) => he.decode(val, {isAttributeValue: true}),//default is a=>a
+        tagValueProcessor : (val, tagName) => he.decode(val), //default is a=>a
+        stopNodes: ["parse-me-as-string"]
+      };
+      
+      // Intermediate obj
+      var tObj = parser.getTraversalObj(cont,options);
+      var jsonObj = parser.convertToJson(tObj,options);
+      releaseName = jsonObj.Project.PropertyGroup[0].AssemblyVersion;
+
+    }
+
 
     // Create a release
     // API Documentation: https://developer.github.com/v3/repos/releases/#create-a-release
